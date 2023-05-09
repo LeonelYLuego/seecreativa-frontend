34cca2cd5336ffdf5a920be306b1a04e4b122029
Leonel Fernández <leonel-leonel-1@hotmail.com>
2023-05-08 19:31:12 -0600

    NavBar finished

--- src/common/components/navbar/index.tsx
+++ src/common/components/navbar/index.tsx
@@ -1,11 +1,62 @@
-import { Menu, Button } from "antd";
+import { Menu, Button, MenuProps } from "antd";
 import { AppstoreOutlined } from "@ant-design/icons";
 import styles from "./style.module.css";
-import Link from "next/link";
 import { PATHS } from "@/common/utils/constants/paths.constant";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
-  const path = PATHS.PRODUCTS.DEFAULT;
+  const router = useRouter()
+  const path = router.pathname;
+
+  const items: MenuProps['items'] = [
+    {
+      key: PATHS.PRODUCTS.DEFAULT,
+      label: (
+        <a href={PATHS.PRODUCTS.DEFAULT}>Productos</a>
+      ),
+      icon: <AppstoreOutlined />,
+
+    },
+    {
+      key: PATHS.ORDERS.DEFAULT,
+      label: (
+        <a href={PATHS.ORDERS.DEFAULT}>Ordenes</a>
+      ),
+      icon: <AppstoreOutlined />,
+    },
+    {
+      key: PATHS.CLIENTS.DEFAULT,
+      label: (
+        <a href={PATHS.CLIENTS.DEFAULT}>Clientes</a>
+      ),
+      icon: <AppstoreOutlined />,
+    },
+    {
+      key: 'sub1',
+      label: 'Otros',
+      icon: <AppstoreOutlined />,
+      children: [
+        {
+          key: PATHS.CLASSIFICATIONS.DEFAULT,
+          label: (
+            <a href={PATHS.CLASSIFICATIONS.DEFAULT}>Clasificaciones</a>
+          ),
+        },
+        {
+          key: PATHS.PRICES.DEFAULT,
+          label: (
+            <a href={PATHS.PRICES.DEFAULT}>Precios</a>
+          ),
+        },
+        {
+          key: PATHS.USERS.DEFAULT,
+          label: (
+            <a href={PATHS.USERS.DEFAULT}>Usuarios</a>
+          ),
+        },
+      ],
+    },
+  ];
 
   return (
     <div className={styles["nav-bar"]}>
@@ -17,13 +68,10 @@ export default function NavBar() {
         mode="horizontal"
         defaultSelectedKeys={[path]}
         className={styles.menu}
-      >
-        <Menu.Item key={PATHS.PRODUCTS.DEFAULT} icon={<AppstoreOutlined />}>
-          <Link href={PATHS.PRODUCTS.DEFAULT}>Productos</Link>
-        </Menu.Item>
-      </Menu>
+        items={items}
+      />
       <div className={styles["log-out"]}>
-        <Button>Salir</Button>
+        <Button type="primary">Salir</Button>
       </div>
     </div>
   );
--- src/common/utils/constants/paths.constant.tsx
+++ src/common/utils/constants/paths.constant.tsx
@@ -1,5 +1,20 @@
 export const PATHS = {
     PRODUCTS: {
         DEFAULT: '/products',
+    },
+    ORDERS: {
+        DEFAULT: '/orders'
+    },
+    CLIENTS: {
+        DEFAULT: '/clients'
+    },
+    CLASSIFICATIONS: {
+        DEFAULT: '/classifications',
+    },
+    PRICES: {
+        DEFAULT: '/prices',
+    },
+    USERS: {
+        DEFAULT: '/users',
     }
 }
